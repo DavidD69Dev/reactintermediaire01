abce6dab5c20840ff1eb69571d808678eb1170e1
John Doe <david.arfi69100@hotmail.fr>
2025-04-10 11:26:36 +0200

    JS - React & Express

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,11 @@ import './App.css'
 import EmployeeCard from './components/EmployeeCard';
 
 
-
-
 function App() {
-
-  const sampleEmployee = {
-    name: {
-      first: "Charlie",
-      last: "Thompson",
-    },
-    email: "charlie.thompson@example.com",
-    picture: {
-      medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-    },
-  };
 
   const getEmployee = () => {
     // Send the request
-    fetch("https://randomuser.me/api?nat=en")
+    fetch("http://localhost:3310/api/employees")
       .then((response) => response.json())
       .then((data) => {
         setEmployee(data.results[0]);
@@ -28,12 +15,12 @@ function App() {
   };
   
 
-  const [employee, setEmployee] = useState(sampleEmployee)
+  const [employee, setEmployee] = useState(null)
 
   return (
     
       <div className="App">
-         <EmployeeCard employee={employee} />
+         { employee !== null && <EmployeeCard employee={employee} />}
          <button type="button" onClick={getEmployee}>Get employee</button>
       </div>
         
